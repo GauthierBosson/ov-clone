5708d8d064ab5e3e264697febe01b2ffd455ebd9
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-10-05 21:10:36 +0200

    📝 chore(home.component): add home component files
    📝 feat(home.component): create HomeComponent class with selector, template, and styles

---
+++ apps/front/src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overseer-clone-home',
+  templateUrl: './home.component.html',
+  styles: [],
+})
+export class HomeComponent {}
