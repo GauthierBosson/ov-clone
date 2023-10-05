7076f0e2784941675ad498ee7159cc6147a2e1a8
Gauthier Bosson <bosson.gauthier27@gmail.com>
2023-10-05 21:10:58 +0200

    📝 chore(sidebar): add sidebar component files
    ✨ feat(sidebar): add sidebar component HTML template with navigation links
    ✨ feat(sidebar): add sidebar component TypeScript file with component definition and template URL

---
+++ apps/front/src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'overseer-clone-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: [],
+})
+export class SidebarComponent {}
